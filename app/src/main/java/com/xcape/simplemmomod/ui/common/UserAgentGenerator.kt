051e90a6b2b2e7fd5b11d2a956ce4d29b6d6eb8d
rhenwinch <rhenwinchester@gmail.com>
2024-12-18 20:20:40 +0800

    feat(data): update UserAgentGenerator

--- app/src/main/java/com/xcape/simplemmomod/ui/common/UserAgentGenerator.kt
+++ app/src/main/java/com/xcape/simplemmomod/ui/common/UserAgentGenerator.kt
@@ -1,24 +1,29 @@
 package com.xcape.simplemmomod.ui.common
 
+import java.util.Locale
+
 object UserAgentGenerator {
     fun generate(): String {
+
         val sb = StringBuilder()
         sb.append("Mozilla/5.0 ")
 
         sb.append(
             String.format(
-                format = "(Linux; Android %d; SM-M%dF)",
-                (9..12).random(),
-                (100..500).random()
+                Locale.US,
+                format = "(Linux; Android %d; SM-A0%dU)",
+                (12..14).random(),
+                (30..50).random()
             )
         )
 
         sb.append(
             String.format(
+                Locale.US,
                 format = " AppleWebKit/537.36 (KHTML, like Gecko) Chrome/%d.0.%d.%d Mobile Safari/537.36",
-                (100..110).random(),
-                (5000..5481).random(),
-                (10..65).random()
+                (131..133).random(),
+                (6000..7000).random(),
+                (100..150).random()
             )
         )
 
